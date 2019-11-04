3340538e9fcfd4070052a3cfd3643a258991763c
Keqiang Li <keqianglli@gmail.com>
2019-11-04 15:58:08 -0500

    fixed dependencies

--- R/gene_mapping.R
+++ R/gene_mapping.R
@@ -6,9 +6,9 @@ SPECIES <- c("hs", "mm", "rn", "dr", "dz")
   mapping_direction <- ifelse(
     from_species == to_species, # if mapping within same species
     from_species,
-    str_c(from_species, "2", to_species)
+    stringr::str_c(from_species, "2", to_species)
   )
-  mapping_dataset_name <- str_interp("${version}_${mapping_direction}_${file_type}_ids")
+  mapping_dataset_name <- stringr::str_interp("${version}_${mapping_direction}_${file_type}_ids")
   if (!mapping_dataset_name %in% names(.gene_mapping_datasets)) {
     stop("Wrong parameters")
   }
@@ -47,31 +47,31 @@ map_genes <- function(from_species = SPECIES,
 
   mapping_table <- .get_mapping_table(from_species, to_species)
 
-  from_id_type_col <- str_c(from_id_type, from_species, sep = "_")
-  to_id_type_col <- str_c(to_id_type, to_species, sep = "_")
+  from_id_type_col <- stringr::str_c(from_id_type, from_species, sep = "_")
+  to_id_type_col <- stringr::str_c(to_id_type, to_species, sep = "_")
 
   if (from_id_type == "ensemblgid") {
     from_id_list <- from_id_list %>%
-      str_split("\\.", n = 2) %>%
-      map_chr(~ .[[1]])
+      stringr::str_split("\\.", n = 2) %>%
+      purrr::map_chr(~ .[[1]])
   }
 
   # produce the result
   result_cols <- c(from_id_type_col, to_id_type_col)
-  from_symbol_col <- str_c("symbol", from_species, sep = "_")
+  from_symbol_col <- stringr::str_c("symbol", from_species, sep = "_")
   if (from_id_type != "symbol") {
     result_cols <- c(result_cols, from_symbol_col) # always add symbol
   }
 
-  to_symbol_col <- str_c("symbol", to_species, sep = "_")
+  to_symbol_col <- stringr::str_c("symbol", to_species, sep = "_")
   if (to_id_type != "symbol") {
     result_cols <- c(result_cols, to_symbol_col) # always add symbol
   }
   mapped_genes <- mapping_table[get(from_id_type_col) %in% from_id_list, ..result_cols]
 
   result_cols <- result_cols[-1]
-  mapped_list <- from_id_list %>%
-    map(function(id) {
+  from_id_list %>%
+    purrr::map(function(id) {
       cur_id_mapped_genes <- mapped_genes[get(from_id_type_col) == id, ..result_cols]
       res <- list(
         "mapped_genes" = cur_id_mapped_genes[[to_id_type_col]]
@@ -84,5 +84,5 @@ map_genes <- function(from_species = SPECIES,
       }
       res
     }) %>%
-    set_names(from_id_list)
+    purrr::set_names(from_id_list)
 }
